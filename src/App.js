15619b816261a0c84eaa9495ddde9aa378346f9c
Mi Yeon Peach <peachkat@uw.edu>
2019-07-20 15:38:20 -0700

    fixed some styling and added gradient

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    memes: [],
+    loading: false,
+    text: '',
+  }
+
+  onSubmit = async (e) => { //getmemes
+    e.preventDefault()
+    this.setState({ loading: true })
+    var key = '7ntM9FAKZtqUr6oxw8G3YQapuWnXavZK'
+    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    var r = await fetch(url)
+    var json = await r.json()
+    this.setState({ memes: json.data, loading: false, text: '' })
+  }
+
+  render() {
+    var { memes, loading, text } = this.state
+    return (
+      <div className="App">
+        <form className="App-header" onSubmit={this.onSubmit}>
+          <input value={text}
+            onChange={e => this.setState({ text: e.target.value })}
+          />
+          <button disabled={loading || !text} type="submit">
+            Search
+            </button>
+        </form>
+        <main>
+          {memes.map(meme => {
+            return <Meme key={meme.id} meme={meme} />
+            function Meme(props){
+              const {meme} = props
+              const url = meme.images.fixed_height.url
+              return (<div className="meme-wrap" onClick={()=>window.open(url, '_blank')}>
+                <img height="200" alt="meme" src={url} />
+              </div>)
+            }
+          })}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
