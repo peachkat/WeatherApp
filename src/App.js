32014d846f6fa60823f7657b6ecc96feb09b68fe
Mi Yeon Peach <peachkat@uw.edu>
2019-07-28 20:07:24 -0700

    Adjusted weather app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,80 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
+//import OutlinedInput from '@material-ui/core/OutlinedInput';
+import TextField from '@material-ui/core/TextField';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import moment from 'moment'
+import { Bar } from 'react-chartjs-2'
 
 class App extends React.Component {
+
   state = {
-    memes: [],
+    weather: null,
     loading: false,
     text: '',
   }
 
-  onSubmit = async (e) => { //getmemes
+  getWeather = async (e) => {
     e.preventDefault()
-    this.setState({loading: true, memes: [] })
-    var key = '7ntM9FAKZtqUr6oxw8G3YQapuWnXavZK'
-    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    this.setState({ loading: true, weather: null })
+    var key = '657d4c335154e8175a3ded8cf74ba786'
+    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.text}&units=imperial&APPID=${key}`
     var r = await fetch(url)
     var json = await r.json()
-    this.setState({ memes: json.data, loading: false, text: '' })
+
+    if (r.status === 200) {
+      this.setState({
+        weather: json.list,
+        loading: false,
+        text: '',
+        error: null
+      })
+    } else {
+      this.setState({
+        error: json.message,
+        loading: false
+      })
+    }
   }
 
   render() {
-    var { memes, loading, text } = this.state
+    var { weather, loading, text, error } = this.state
+    var data
+
+    if (weather) {
+      data = {
+        labels: weather.map(w => moment(w.dt * 1000).format('11 hh:mm a')), //formats time properly with moment
+        datasets: [{
+          label: 'Tempurature',
+          borderWidth: 1,
+          data: weather.map(w => w.main.temp), //takes temperature out of the main in JSON data
+        }]
+      }
+    }
+    console.log(data)
     return (
       <div className="App">
         <form className="App-header" onSubmit={this.onSubmit}>
-          <OutlinedInput value={text}
+          <TextField value={text}
+            autoFocus
+            variant="outlined"
+            label="Search for Weather"
             onChange={e => this.setState({ text: e.target.value })}
+            style={{ width: '100%', marginLeft: 8 }}
           />
           <Button disabled={loading || !text} variant="contained" color="primary">
             Search
-            </Button>
+          </Button>
         </form>
         {loading && <LinearProgress />}
         <main>
-          {memes.map(meme => {
-            return <Meme key={meme.id} meme={meme} />
-            function Meme(props) {
-              const { meme } = props
-              const url = meme.images.fixed_height.url
-              return (<div className="meme-wrap" onClick={() => window.open(url, '_blank')}>
-                <img height="200" alt="meme" src={url} />
-              </div>)
-            }
-          })}
+          {data && <Bar
+            data={data}
+            width={800}
+            height={400}
+          />}
+          {error}
         </main>
       </div>
     );
